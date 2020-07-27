a984e38d2c40e1cfbbd9d040f967774085067f7a
Roland Tritsch <roland@tritsch.org>
2020-07-27 16:54:34 +0100

    Add script to run curls.

---
+++ scripts/run.sh
@@ -0,0 +1,12 @@
+fsm=http://localhost:4000
+id=$(curl --silent ${fsm}/create)
+curl --write-out "\n" ${fsm}/retrieve/${id}
+curl --write-out "\n" ${fsm}/update/${id}?transition=start
+curl --write-out "\n" ${fsm}/retrieve/${id}
+curl --write-out "\n" ${fsm}/update/${id}?transition=run
+curl --write-out "\n" ${fsm}/retrieve/${id}
+curl --write-out "\n" ${fsm}/update/${id}?transition=done
+curl --write-out "\n" ${fsm}/retrieve/${id}
+curl --write-out "\n" ${fsm}/update/${id}?transition=done
+curl --write-out "\n" ${fsm}/retrieve/1234
+curl --write-out "\n" ${fsm}/delete/${id}
