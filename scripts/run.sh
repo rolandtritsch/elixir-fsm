0bed8a095dd73fa4f0ebcabda9ee04679b3d5b9a
Roland Tritsch <roland@tritsch.org>
2020-09-26 19:23:07 +0100

    Change the script to show the id (for debugging) ...
    ... and test more states (and work around a/the bug in Ecto.FSM).

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,12 +1,17 @@
 fsm=http://localhost:4000
 id=$(curl --silent ${fsm}/create)
+echo ${id}
+curl --write-out "\n" ${fsm}/retrieve/${id}
+curl --write-out "\n" ${fsm}/update/${id}?transition=schedule
 curl --write-out "\n" ${fsm}/retrieve/${id}
 curl --write-out "\n" ${fsm}/update/${id}?transition=start
 curl --write-out "\n" ${fsm}/retrieve/${id}
 curl --write-out "\n" ${fsm}/update/${id}?transition=run
 curl --write-out "\n" ${fsm}/retrieve/${id}
-curl --write-out "\n" ${fsm}/update/${id}?transition=done
+curl --write-out "\n" ${fsm}/update/${id}?transition=process
 curl --write-out "\n" ${fsm}/retrieve/${id}
-curl --write-out "\n" ${fsm}/update/${id}?transition=done
+curl --write-out "\n" ${fsm}/update/${id}?transition=success
+curl --write-out "\n" ${fsm}/retrieve/${id}
+curl --write-out "\n" ${fsm}/update/${id}?transition=hokuspokus
 curl --write-out "\n" ${fsm}/retrieve/$(uuidgen)
 curl --write-out "\n" ${fsm}/delete/${id}
