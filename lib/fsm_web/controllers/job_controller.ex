2d9768a6491d822eb15b954953926f9bb66cf91a
Roland Tritsch <roland@tritsch.org>
2020-11-18 17:07:46 +0000

    Add the retrieve endpoint.

--- lib/fsm_web/controllers/job_controller.ex
+++ lib/fsm_web/controllers/job_controller.ex
@@ -9,9 +9,23 @@ defmodule FsmWeb.JobController do
   defmodule Schema do
     require OpenApiSpex
 
+    defmodule ErrorResponse do
+      OpenApiSpex.schema(%{
+            description: "Something went wrong. Return the reason.",
+            type: :object,
+            properties: %{
+              error: %OpenApiSpex.Schema{type: :string, description: "Error"}
+            },
+            required: [:error],
+            example: %{
+              "error" => "invalid id"
+            }
+      })
+    end
+
     defmodule CreateResponse do
       OpenApiSpex.schema(%{
-            description: "Created a/the job.",
+            description: "Return id of the created job.",
             type: :object,
             properties: %{
               created: %OpenApiSpex.Schema{type: :string, description: "Created", format: :uuid}
@@ -22,9 +36,25 @@ defmodule FsmWeb.JobController do
             }
       })
     end
+
+    defmodule RetrieveResponse do
+      OpenApiSpex.schema(%{
+            description: "Return the current status of the given job.",
+            type: :object,
+            properties: %{
+              status: %OpenApiSpex.Schema{type: :string, description: "Status", pattern: ~r/(started)|(initialized)|(scheduled)|(running)|(processing)|(failed)/}
+            },
+            required: [:status],
+            example: %{
+              "status" => "started"
+            }
+      })
+    end
   end
   
   @doc """
+  /create 
+
   Create a job. Return the job id.
   """
   @doc responses: [
@@ -35,26 +65,37 @@ defmodule FsmWeb.JobController do
     
     conn
     |> put_resp_content_type("text/plain")
-    |> send_resp(201, id)
+    |> send_resp(:created, id)
   end
 
-  @doc false
+  @doc """
+  /retrieve/:id
+
+  Retrieve the current status of the given job.
+  """
+  @doc parameters: [
+    path: [in: :path, type: :string, required: :true, description: "Id of the job"]
+  ]
+  @doc responses: [
+    ok: {"Status", "text/plain", Schema.RetrieveResponse},
+    bad_request: {"Error", "text/plain", Schema.ErrorResponse}
+  ]
   def retrieve(conn, %{"id" => id}) do
     case PersistentJob.retrieve(id) do
       {:ok, state} -> 
         conn
         |> put_resp_content_type("text/plain")
-        |> send_resp(200, state)
+        |> send_resp(:ok, state)
         
       {:error, :invalid_id} ->
          conn 
         |> put_resp_content_type("text/plain")
-        |> send_resp(400, "invalid id")
+        |> send_resp(:bad_request, "invalid id")
         
       error ->
          conn 
         |> put_resp_content_type("text/plain")
-        |> send_resp(400, inspect(error))
+        |> send_resp(:bad_request, inspect(error))
     end
   end
 
@@ -64,22 +105,22 @@ defmodule FsmWeb.JobController do
       {:ok, state} -> 
         conn
         |> put_resp_content_type("text/plain")
-        |> send_resp(200, state)
+        |> send_resp(:ok, state)
         
       {:error, :invalid_id} ->
          conn 
         |> put_resp_content_type("text/plain")
-        |> send_resp(400, "invalid id")
+        |> send_resp(:bad_request, "invalid id")
         
       {:error, :invalid_transition} ->
          conn 
         |> put_resp_content_type("text/plain")
-        |> send_resp(400, "invalid transition")
+        |> send_resp(:bad_request, "invalid transition")
         
       error ->
          conn 
         |> put_resp_content_type("text/plain")
-        |> send_resp(400, inspect(error))
+        |> send_resp(:bad_request, inspect(error))
     end
   end
 
@@ -89,17 +130,17 @@ defmodule FsmWeb.JobController do
       {:ok, _id} -> 
         conn
         |> put_resp_content_type("text/plain")
-        |> send_resp(200, "deleted")
+        |> send_resp(:ok, "deleted")
         
       {:error, :invalid_id} ->
          conn 
         |> put_resp_content_type("text/plain")
-        |> send_resp(400, "invalid id")
+        |> send_resp(:bad_request, "invalid id")
         
       error ->
          conn 
         |> put_resp_content_type("text/plain")
-        |> send_resp(400, inspect(error))
+        |> send_resp(:bad_request, inspect(error))
     end
   end
 end
