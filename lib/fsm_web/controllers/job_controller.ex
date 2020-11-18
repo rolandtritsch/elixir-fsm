c9d17f1d9725d3881cee074bdc7aecf1f609579e
Roland Tritsch <roland@tritsch.org>
2020-11-18 10:56:56 +0000

    Adding the create endpoint.

--- lib/fsm_web/controllers/job_controller.ex
+++ lib/fsm_web/controllers/job_controller.ex
@@ -1,6 +1,37 @@
 defmodule FsmWeb.JobController do
+  @moduledoc """
+  All endpoints to manage jobs.
+  """
+  
   use FsmWeb, :controller
+  use OpenApiSpex.Controller
 
+  defmodule Schema do
+    require OpenApiSpex
+
+    defmodule CreateResponse do
+      OpenApiSpex.schema(%{
+            description: "Created a/the job.",
+            type: :object,
+            properties: %{
+              created: %OpenApiSpex.Schema{type: :string, description: "Created", pattern: ~r/[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}/}
+            },
+            required: [:created],
+            example: %{
+              "created" => "6a2f41a3-c54c-fce8-32d2-0324e1c32e22"
+            }
+      })
+    end
+  end
+  
+  @doc """
+  /create
+
+  Create a job. Return the job id.
+  """
+  @doc responses: [
+    created: {"Created", "text/plain", Schema.CreateResponse}
+  ]
   def create(conn, _params) do
      {:ok, id} = PersistentJob.create() 
     
@@ -9,6 +40,7 @@ defmodule FsmWeb.JobController do
     |> send_resp(201, id)
   end
 
+  @doc false
   def retrieve(conn, %{"id" => id}) do
     case PersistentJob.retrieve(id) do
       {:ok, state} -> 
@@ -28,6 +60,7 @@ defmodule FsmWeb.JobController do
     end
   end
 
+  @doc false
   def update(conn, %{"id" => id, "transition" => transition}) do
     case PersistentJob.update(id, transition) do
       {:ok, state} -> 
@@ -51,7 +84,8 @@ defmodule FsmWeb.JobController do
         |> send_resp(400, inspect(error))
     end
   end
-  
+
+  @doc false
   def delete(conn, %{"id" => id}) do
     case PersistentJob.delete(id) do
       {:ok, _id} -> 
