899ac13014e93a47effac5bf7dede0b1ca24d1c4
Roland Tritsch <roland@tritsch.org>
2020-11-19 08:29:45 +0000

    Run spectral when generating the spec.

--- scripts/api.sh
+++ scripts/api.sh
@@ -2,4 +2,5 @@
 
 mix openapi.spec.json --spec FsmWeb.ApiSpec && \
     python -m json.tool openapi.json > api/openapi.json && \
-    rm openapi.json
+    rm openapi.json && \
+    spectral lint api/openapi.json
