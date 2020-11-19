ad185fb2be462b5407e1a34d77816dd064f0b450
Roland Tritsch <roland@tritsch.org>
2020-11-19 08:36:27 +0000

    Add api task (as an alias).

--- mix.exs
+++ mix.exs
@@ -60,7 +60,8 @@ defmodule Fsm.MixProject do
       setup: ["deps.get", "ecto.setup", "cmd npm install --prefix assets"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"]
+      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
+      api: ["cmd ./scripts/api.sh"]
     ]
   end
 end
